4975f4b0180b3650f1099400200a505701a38c86
Malick Diakite <malickdiakite496@gmail.com>
2020-02-28 02:25:53 +0530

    Style the boxes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function fillBar(sec) {
   // Select the bar
   const bar = document.querySelector(".bar");
-
+  const numat = document.querySelector(".num");
   // Percent where to start animating
   let atPercent = 0;
 
@@ -13,7 +13,9 @@ function fillBar(sec) {
     // Set the width style of the bar
     // to atPercent
     bar.style.width = `${atPercent}%`;
-
+    bar.style.backgroundColor = "green";
+    //show the percentage
+    numat.textContent = `${atPercent}%`;
     // Make sure that atPercent is not equal 100
     if (atPercent >= 100) {
       // if it is  then clear setInterval function
